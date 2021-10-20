eba1b225b0b4bd17f6174447def6f1127eede857
Umar-37 <omaralhussan@outlook.com>
2021-10-20 13:32:52 +0300

    add tailwind

--- public/index.js
+++ public/index.js
@@ -7,33 +7,3 @@ db.collection('user').doc('*').get().then(snapshot => {
   console.log(snapshot.data())
 })
 
-class square {
-  constructor(width) {
-    this.height = width;
-    this.width = width;
-  }
-  static equ(a, b) {
-    return this;
-  }
-  insideSquare(){
-    return 99;
-  }
-
-}
-class omar extends square{
-  constructor(width,name,age){
-    super(width)
-    this.name=name;
-    this.age=age;
-    super.insideSquare() *2
-  }
-
-  fun(){
-    return this.insideSquare();
-  }
-}
-
-let sq1 = new square(4)
-let sq2 = new square(2)
-let om1 = new omar(24,"omar",24)
-log(om1.)
