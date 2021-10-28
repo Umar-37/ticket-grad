fa918aae70473af6febe9f03c7d3e9c132a17872
Umar-37 <omaralhussan@outlook.com>
2021-10-28 19:36:42 +0300

    change something idk

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,39 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+      fill: theme => ({
+
+       'red': theme('colors.red.500'),
+
+       'white': theme('colors.white.100'),
+
+       'green': theme('colors.green.500'),
+
+       'blue': theme('colors.blue.500'),
+
+     }),
+         gradientColorStops: theme => ({
+
+
+       'primary': '#3490dc',
+
+       'secondary': '#ffed4a',
+
+       'danger': '#e3342f',
+
+       'from1':'#FFEFBA',
+       'to1':'#FFFFFF',
+
+       'from2':'#005AA7',
+       'to2':'#FFFDE4',
+
+      }),
     extend: {
       backgroundImage:{
-        //'img-for-backg':"url('../images/sun.png')",
-        'img-for-backg':"url('../images/sun.png')"
+        'img-for-banner':"url('../images/Banner.png')",
+        'img-for-salh':"url('../images/salh.png')"
+        ,'img-for-backg':"url('../images/sun.png')"
+        ,'img-for-blue':"url('../images/blue.jpeg')"
         
       }
     },
