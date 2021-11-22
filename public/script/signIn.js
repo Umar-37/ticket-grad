dba854c0aadc3513b558116ac0b2399cb77672bf
Umar-37 <omaralhussan@outlook.com>
2021-11-22 20:21:27 +0300

    when password wrong clear the input field

--- public/script/signIn.js
+++ public/script/signIn.js
@@ -13,12 +13,11 @@ var count=0;
 form.addEventListener('submit', e => {
     e.preventDefault()
     auth.signInWithEmailAndPassword(email.value, pass.value).then(cred => {
-        console.log(cred.user)
         window.location = 'indexHisham.html'
         //form.reset()
     }).catch(error => {
         if (error.code == 'auth/wrong-password') {
-            popUp('Wrong password. Try again')
+            popUp('Wrong password. Try again',true)
         }
         else if (error.code == 'auth/too-many-requests') {
             popUp('Accouont temporary suspended, try after one hour')
@@ -32,9 +31,11 @@ form.addEventListener('submit', e => {
     })
 
 })
-function popUp(mass) {
+function popUp(mass,passwrod) {
     const tempNode = document.importNode(temp.content, true)
     const popUp=document.querySelectorAll('[data-new-toFade]')[count]
+
+    passwrod ? pass.value='':false;
 
     if(popUp){
     popUp.classList.toggle('hidden')
