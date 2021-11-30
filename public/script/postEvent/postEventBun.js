e79100c3727528c82d923bd29aa44c5c635af445
Umar-37 <omaralhussan@outlook.com>
2021-11-30 16:29:59 +0300

    working on form submission

--- public/script/postEvent/postEventBun.js
+++ public/script/postEvent/postEventBun.js
@@ -21,29 +21,122 @@ var _default = {
 };
 exports["default"] = _default;
 },{}],3:[function(require,module,exports){
+let log = console.log;
+let db = firebase.firestore();
+let auth = firebase.auth()
+//////////////////////////////
+const formData = new FormData(document.querySelector('form'))
+ window.onload = function() {
+      initApp();
+    };
+//////////////////////////////
+function initApp(){
+  auth.onAuthStateChanged(function(user) {
+    if(user){
+document.querySelector('form').addEventListener('submit', e => {
+  e.preventDefault()
+  dataUser = {} //define it as globel object to access it in code below
+  looping(user)
+  createDoc()
+  // console.log(dp.selectedDates);
+  // console.log(formData);
+})
+    }else window.location='indexHisham.html'  //enable this
+ })
+
+  }
+//-------function area------------------
+function looping(user) {
+  for (var pair of formData.entries()) {
+    if (pair[0] == 'date') {
+      //idk take it from here https://stackoverflow.com/questions/12482961/change-values-in-array-when-doing-foreach
+      dp.selectedDates.forEach(function (part, index) {
+        //must convert it to a string by toString ,then remove what we dont want by replace function 
+        this[index] = part.toString().replace('00:00:00 GMT+0300 (Arabian Standard Time)', '');
+      }, dp.selectedDates); // use arr as this
+      pair[1] = dp.selectedDates
+    }
+    dataUser[pair[0]] = pair[1];
+  }
+  dataUser.owner=user.uid
+}
+function createDoc() {
+  delete dataUser.file //becasue it contain the img data and firebase does not store this type of object
+  db.collection("events").add(dataUser)
+    .then(() => {
+      console.log("Document successfully written!");
+    })
+    .catch((error) => {
+      console.error("Error writing document: ", error.code);
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //import {AirDatepicker}from "./../node_modules/air-datepicker/index.es";
-const en=require('air-datepicker/locale/en').default
-const AirDatepicker=require('air-datepicker')
+const en = require('air-datepicker/locale/en').default
+const AirDatepicker = require('air-datepicker')
 //import en from "air-datepicker/locale/en";
 
 
-let startDate = new Date('2021-07-20');
-let tomorrowDate = new Date(startDate);
-tomorrowDate.setDate(tomorrowDate.getDate()+1)
-
+///
+var dateObj = new Date();
+let month = dateObj.getUTCMonth() + 1; //months from 1-12
+let day = dateObj.getUTCDate();
+let year = dateObj.getUTCFullYear();
+let today = year + "/" + month + "/" + day;
 let options = {
   locale: en,
-    startDate,
-    multipleDates: true,
-    selectedDates: [startDate, '2021-07-25', 1626307200000]
+  minDate: today,
+  today,
+  multipleDates: true
+  // ,  selectedDates: [today]
 }
-
-let dp = new AirDatepicker("#dp", options);
+var dp = new AirDatepicker("#dp", options);
 ////////////////////////////////////////////////////////
 //    code that handle image viewing
 
 class ImageInputField extends HTMLElement {
-  connectedCallback() { 
+  connectedCallback() {
     // Configure click listeners for the two buttons
     // and a change listener for the input field
     this.configureListeners()
@@ -58,7 +151,7 @@ class ImageInputField extends HTMLElement {
     this.input.style.display = "none"
 
     // Restrict the input field to images only
-    this.input.accept="image/*"
+    this.input.accept = "image/*"
   }
 
   get input() {
@@ -108,9 +201,11 @@ class ImageInputField extends HTMLElement {
       this.removeImage()
     })
   }
-} 
+}
 
 // Register our custom element with the CustomElementRegistry
 customElements.define('image-input-field', ImageInputField)
+/////////////////////////////////////////////////////////
+
 
 },{"air-datepicker":1,"air-datepicker/locale/en":2}]},{},[3]);
