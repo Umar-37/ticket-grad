dc694d6c238a83cea621956d93b83d823453ff97
Umar-37 <omaralhussan@outlook.com>
2021-12-15 21:58:25 +0300

    tickets in eventPage

--- public/script/eventPage.js
+++ public/script/eventPage.js
@@ -4,6 +4,9 @@ let auth = firebase.auth()
 ////////
 const container = document.getElementById('container')
 const eventTemplate = document.getElementById('template')
+///ticket type template
+//const ticket=document.getElementById('type-template')
+///
 const LOCAL_STORAGE_EVENT_ID = 'eventId'
 var eventId = JSON.parse(localStorage.getItem(LOCAL_STORAGE_EVENT_ID))
     || [];
@@ -15,7 +18,7 @@ auth.onAuthStateChanged(user => {
         getData()
     }
     else {
-    document.getElementById('sign/login').classList.toggle('hidden')
+        document.getElementById('sign/login').classList.toggle('hidden')
         log('sign out i am in the event page')
 
         //  window.location='index.html'
@@ -23,7 +26,9 @@ auth.onAuthStateChanged(user => {
 })
 //////////
 function render(doc) {
+    //page template
     const temp = document.importNode(eventTemplate.content, true)
+
     const name = temp.querySelector('#name')
     const location = temp.querySelector('#loacation')
     const date = temp.querySelector('#date')
@@ -51,9 +56,39 @@ function render(doc) {
     document.getElementById('img').style.backgroundSize = 'cover'
     document.getElementById('img').style.backgroundPosition = 'center'
 }
+function renderTicket(doc) {
+    //ticket template
+    //const ticketTemp = document.importNode(ticket.content, true)
+    //ticket types
+    let types = doc.data()
+    if (types.vipTicket[0]) {
+        crateTicket(types.vipTicket[0],types.vipTicket[1])
+        crateTicket(types.silTicket[0],types.silTicket[1])
+        crateTicket(types.regTicket[0],types.regTicket[1])
+    } else if (types.silTicket[0]) {
+                crateTicket(types.silTicket[0],types.silTicket[1])
+                crateTicket(types.regTicket[0],types.regTicket[1])
+    } else {
+                crateTicket(types.regTicket[0],types.regTicket[1])
+        //just one ticket
+    }
+}//end loop
 function getData() {
     db.collection('events').doc(eventId[0]).get().then(doc => {
         render(doc)
+        renderTicket(doc)
         //redirect()
     })
+}
+function crateTicket(type, price) {
+    const typeContainer = document.getElementById('type-container')
+    let tr = document.createElement('TR')
+    let th = document.createElement('TH')
+    let td = document.createElement('TD')
+
+    th.innerText = type
+    td.innerText = price
+    tr.append(th)
+    tr.append(td)
+    typeContainer.append(tr)
 }
